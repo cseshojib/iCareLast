4cd3deffdf18586b004345037c9de829e4c42112
Suuny <sunny_mhs@hotmail.com>
2015-11-23 23:08:15 +0600

    i add Profile Related db classes

---
+++ app/src/main/java/com/example/shojib/project_moon/GeneralProfile/GeneralProfileDataBaseQuery.java
@@ -0,0 +1,145 @@
+package com.example.shojib.project_moon.GeneralProfile;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+
+import com.example.shojib.project_moon.DBHelperPackage.DbHelper;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Suuny on 11/23/2015.
+ */
+public class GeneralProfileDataBaseQuery {
+    private Context context;
+    private DbHelper mPDbHelper;
+    private SQLiteDatabase mSqLiteDatabase;
+
+    private String[] allColumns={
+            DbHelper.COLUMN_GENERAL_INFO_USER_ID,
+            DbHelper.COLUMN_GENERAL_INFO_PROFILE_NAME,
+            DbHelper.COLUMN_GENERAL_INFO_AGE,
+            DbHelper.COLUMN_GENERAL_INFO_BLOOD_GROUP,
+            DbHelper.COLUMN_GENERAL_INFO_GENDER,
+            DbHelper.COLUMN_GENERAL_INFO_HEIGHT,
+            DbHelper.COLUMN_GENERAL_INFO_WEIGHT,
+            DbHelper.COLUMN_GENERAL_INFO_BLOOD_PRESSURE,
+            DbHelper.COLUMN_GENERAL_INFO_DISEASE
+
+    };
+    public GeneralProfileDataBaseQuery(Context context) {
+        this.context = context;
+        this.mPDbHelper = new DbHelper(context,DbHelper.DATABASE_NAME,null,DbHelper.DATABASE_VERSION);
+        try {
+            open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void open() throws SQLException {
+        mSqLiteDatabase=mPDbHelper.getWritableDatabase();
+    }
+    private void close()
+    {
+        mPDbHelper.close();
+    }
+    public  void CreateNewProfile( String profileName, int age, String bloodGroup, String gender, float height, float weight, float bloodPressure, String disease)
+    {
+        ContentValues values=new ContentValues();
+
+
+        values.put(DbHelper.COLUMN_GENERAL_INFO_PROFILE_NAME,profileName);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_AGE,age);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_BLOOD_GROUP,bloodGroup);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_GENDER,gender);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_HEIGHT,height);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_WEIGHT,weight);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_BLOOD_PRESSURE,bloodPressure);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_DISEASE, disease);
+
+        long insertId=mSqLiteDatabase.insert(DbHelper.TABLE_NAME_GENERAL_INFO, null, values);
+        mSqLiteDatabase.query(DbHelper.TABLE_NAME_GENERAL_INFO, allColumns, DbHelper.COLUMN_GENERAL_INFO_USER_ID + " = " + insertId, null, null, null, null);
+
+
+
+    }
+
+    public List<GeneralProfileModule> getAllProfile()
+    {   List<GeneralProfileModule> generalProfileModuleList=new ArrayList<>();
+        Cursor cursor=mSqLiteDatabase.query(DbHelper.TABLE_NAME_GENERAL_INFO, allColumns, null, null, null, null, null);
+        if(cursor!=null)
+        {
+            try {
+                cursor.moveToFirst();
+                while (!cursor.isAfterLast())
+                {
+                    GeneralProfileModule generalProfileModule=cursorToProfileModule(cursor);
+                    generalProfileModuleList.add(generalProfileModule);
+                    cursor.moveToNext();
+
+                }
+            }finally {
+                cursor.close();
+            }
+        }
+        return generalProfileModuleList;
+
+
+    }
+
+    private GeneralProfileModule cursorToProfileModule(Cursor cursor) {
+
+        GeneralProfileModule generalProfileModule=new GeneralProfileModule();
+        generalProfileModule.setUserId(cursor.getInt(0));
+        generalProfileModule.setProfileName(cursor.getString(1));
+        generalProfileModule.setAge(cursor.getInt(2));
+        generalProfileModule.setBloodGroup(cursor.getString(3));
+        generalProfileModule.setGender(cursor.getString(4));
+        generalProfileModule.setHeight(cursor.getFloat(5));
+        generalProfileModule.setWeight(cursor.getFloat(6));
+        generalProfileModule.setBloodPressure(cursor.getFloat(7));
+        generalProfileModule.setDisease(cursor.getString(8));
+
+
+        return generalProfileModule;
+    }
+    private void UpDateProfileByProfileId(long userId, String profileName, int age, String bloodGroup, String gender, float height, float weight, float bloodPressure, String disease)
+    {
+        try {
+            open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ContentValues values=new ContentValues();
+
+        values.put(DbHelper.COLUMN_GENERAL_INFO_PROFILE_NAME,profileName);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_AGE, age);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_BLOOD_GROUP, bloodGroup);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_GENDER, gender);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_HEIGHT, height);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_WEIGHT, weight);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_BLOOD_PRESSURE, bloodPressure);
+        values.put(DbHelper.COLUMN_GENERAL_INFO_DISEASE, disease);
+         mSqLiteDatabase.update(DbHelper.TABLE_NAME_GENERAL_INFO, values, DbHelper.COLUMN_GENERAL_INFO_USER_ID + " = " + userId, null);
+   close();
+    }
+
+    public void profileDeletByProfileId(long ePId){
+        try {
+            open();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        mSqLiteDatabase.delete(DbHelper.TABLE_NAME_GENERAL_INFO,DbHelper.COLUMN_GENERAL_INFO_USER_ID+ " = "+ePId,null);
+        close();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/shojib/project_moon/GeneralProfile/GeneralProfileModule.java
@@ -0,0 +1,108 @@
+package com.example.shojib.project_moon.GeneralProfile;
+
+import java.io.Serializable;
+
+/**
+ * Created by Suuny on 11/23/2015.
+ */
+public class GeneralProfileModule implements Serializable {
+
+    private static final long serilVersionUID=-7406082437623008161L;
+
+   private int userId;
+   private String profileName;
+   private int age ;
+   private String bloodGroup ;
+   private String gender;
+   private float height;
+   private float weight;
+   private float bloodPressure;
+   private String disease;
+    public GeneralProfileModule(){
+
+    }
+
+    public GeneralProfileModule(int userId, String profileName, int age, String bloodGroup, String gender, float height, float weight, float bloodPressure, String disease) {
+        this.userId = userId;
+        this.profileName = profileName;
+        this.age = age;
+        this.bloodGroup = bloodGroup;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.bloodPressure = bloodPressure;
+        this.disease = disease;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getBloodPressure() {
+        return bloodPressure;
+    }
+
+    public void setBloodPressure(float bloodPressure) {
+        this.bloodPressure = bloodPressure;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+}
